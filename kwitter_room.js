6ac5faf0359517326fc27aad2388ecef74b346c6
BaidLavanya <75931821+BaidLavanya@users.noreply.github.com>
2021-05-14 22:03:11 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCeJMT5NRLJZZBdb8FeEsJyN5FWKjA9Mug",
+    authDomain: "kwitter-6abd1.firebaseapp.com",
+    databaseURL: "https://kwitter-6abd1-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "kwitter-6abd1",
+    storageBucket: "kwitter-6abd1.appspot.com",
+    messagingSenderId: "163093228903",
+    appId: "1:163093228903:web:d7e78020c20e21b85953c2",
+    measurementId: "G-GD03653ZYN"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
